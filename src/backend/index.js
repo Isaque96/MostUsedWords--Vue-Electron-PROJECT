f654f861dc18e15519033bfbaf338a75054a1ff5
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-30 20:03:55 -0300

    Working with the Data in BackEnd

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,10 +1,9 @@
 const { ipcMain } = require('electron');
 const pathsToRows = require('./pathsToRow');
+const prepareData = require('./prepareData');
 
 ipcMain.on('process-subtitles', (event, paths) => {
-  console.log(paths);
-
-  pathsToRows(paths).then(rows => console.log(rows)).then(() => {
+  pathsToRows(paths).then(rows => prepareData(rows)).then(words => console.log(words)).then(() => {
     event.reply('process-subtitles', [    
       { name: "you", amount: 900 },
       { name: "he", amount: 853 },
