13361311c0ef239bf4fe9d1e48a3ec53dae3a8a8
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-30 19:05:19 -0300

    Receive File Method Implemented

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,55 @@
 <template>
   <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as Legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
     <div class="pill">
-      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
     </div>
   </v-container>
 </template>
 
 <script>
-import Pill from './Pill.vue'
+import Pill from "./Pill.vue";
 
 export default {
   components: { Pill },
-  data: function() {
+  data: function () {
     return {
+      files: [],
       groupedWords: [
-        { name: 'you', amount: 900 },
-        { name: 'he', amount: 853 },
-        { name: 'i', amount: 1234 }
-      ]
-    }
-  }
-}
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+        { name: "i", amount: 1234 },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
 </script>
 
 <style>
-  .pill {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-  }
+.pill {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
 </style>
