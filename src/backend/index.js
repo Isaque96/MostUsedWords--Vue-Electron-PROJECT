117b4191c014adad478d4c5ce88bd81857b97570
Isaque Schuwarte <isaque_schuwarte@hotmail.com>
2021-09-30 21:01:01 -0300

    Project Closing

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,16 +1,9 @@
 const { ipcMain } = require('electron');
 const pathsToRows = require('./pathsToRow');
 const prepareData = require('./prepareData');
+const groupWords = require('./groupWords');
 
 ipcMain.on('process-subtitles', (event, paths) => {
-  pathsToRows(paths).then(rows => prepareData(rows)).then(words => console.log(words)).then(() => {
-    event.reply('process-subtitles', [    
-      { name: "you", amount: 900 },
-      { name: "he", amount: 853 },
-      { name: "i", amount: 1234 },  
-      { name: "she", amount: 876 },  
-      { name: "our", amount: 133 },  
-      { name: "house", amount: 23 },  
-    ])    
-  })
+  pathsToRows(paths).then(rows => prepareData(rows)).then(words => groupWords(words))
+  .then(groupedWords => event.reply('process-subtitles', groupedWords))
 });
